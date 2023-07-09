5047454e6b329a2e6060ed3f8205df19a8188f7c
Shourov Foisal <shourovfoisal@gmail.com>
2023-07-09 16:57:59 +0600

    Created the middleware. It has to be directly inside of the src directory.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <Link href={`/`}>Home Page</Link>
+    </div>
+  )
+}
+
+export default About
---
+++ src/middleware.js
@@ -0,0 +1,8 @@
+/**
+ * function name should be middleware
+ * any other name would also work
+ * but in that case, its export has to be default
+ */
+export function middleware() {
+    console.log("hello from the middleware.. again..");
+}
